283a3cd4b0ec433ac14ea343bebb520adaaf4285
unknown <verwiel.code@gmail.com>
2021-10-14 15:55:07 -0600

    learned about tsconfig and options, set up src and dist

---
+++ src/basics.ts
@@ -0,0 +1,16 @@
+// const num1 = 1.5
+// const num2 = 5
+// const printResult = true
+// const resultPhrase = 'Result is: '
+
+// function add(n1: number, n2: number, showResult: boolean, phrase: string){
+//   // needs to be calculated before being added with a string, otherwise it concats all
+//   let result = n1 + n2
+//   if(showResult){
+//     console.log(phrase + result)
+//   } else { 
+//     return result
+//   }
+// }
+
+// add(num1, num2, printResult, resultPhrase)
